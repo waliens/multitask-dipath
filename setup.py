5975b1665dbac6288016cdccc018378abc2b58e4
Waliens <r.mormont@uliege.be>
2019-11-25 17:25:15 +0100

    add numpy as dependency

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ],
-    install_requires=['torch', 'torchvision'],
+    install_requires=['torch', 'torchvision', 'numpy'],
     license='LICENSE'
 )
 
